7e0008525616e3e63899686bdc8c6572295df249
nouval0425 <ghifarynopal@gmail.com>
2026-07-09 16:43:58 +0700

    add : login.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["login"])){
+    header("Location: login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,6 +30,8 @@
             <td><a href="contact.php">Contact</a></td>
             <td><a href="mahasiswa.php">Data Mahasiswa</a></td>
             <td><a href="register.php">Register</a></td>
+            <td><a href="login.php">Login</a></td>
+            
         </tr>
     </table>
  </div>
--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+require 'fungsi.php';
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = mysqli_query($koneksi,
+    "SELECT * FROM user WHERE username='$username'");
+
+    if(mysqli_num_rows($query) == 1){
+
+        $user = mysqli_fetch_assoc($query);
+
+        if(password_verify($password, $user['password'])){
+
+            $_SESSION['login'] = true;
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+
+            header("Location: index.php");
+            exit;
+
+        }else{
+
+            echo "<script>alert('Password salah');</script>";
+
+        }
+
+    }else{
+
+        echo "<script>alert('Username tidak ditemukan');</script>";
+
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +46,32 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h1>LOGIN</h1>
+
+<form method="post">
+
+    Username <br>
+    <input type="text" name="username" required>
+
+    <br><br>
+
+    Password <br>
+    <input type="password" name="password" required>
+
+    <br><br>
+
+    <button type="submit" name="login">
+        Login
+    </button>
+
+</form>
+
 </body>
 </html>
